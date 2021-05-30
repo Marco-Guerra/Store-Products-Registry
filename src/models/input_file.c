f927461d428d571e10ab57cb8490f68ea843006c
darkvictor13 <01victoralmeida@gmail.com>
2021-05-30 15:22:09 -0300

    inicio leitura

--- src/models/input_file.c
+++ src/models/input_file.c
@@ -1,21 +1,69 @@
+/**
+ * @file input_file.c
+ * @author Victor Emanuel Almeida (victoralmeida2001@hotmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 30/05/2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #include "input_file.h"
+
+Product * readProductDataFromInputFile(FILE *input) {
+    Product *p = (Product *)malloc(sizeof(Product));
+    fscanf(input, "%d%*c", &p->code);
+    printf("%d\n", p->code);
+    fscanf(input, "%[^;]%*c", p->name);
+    fscanf(input, "%d%*c", &p->number);
+    fscanf(input, "%f%*c", &p->value);
+    fscanf(input, "%[^;]%*[^\n]%*r", p->local);
+    printf("Dados do produto\n\n");
+    printProduct(p);
+    return p;
+}
 
 void loadInputFile(char *inputPath, FILE *dataFile) {
     FILE *inputFile = fopen(inputPath, "r");
-    char *line[MAX_ENTRY_LINE];
+    if (inputFile == NULL) {
+        printf("Falha na abertura do arquivo\n");
+        return ;
+    }
+    
+    //char *line[MAX_ENTRY_LINE];
     char c;
-    while(0) {
-        //fscanf();
+    Product read;
+    while(fscanf(inputFile, "%c%*c", &c) != EOF) {
+        printf("Estou no caractere %c\n", c);
 
         switch(c) {
-            case 'I': insertFornFile(line);
+            case INPUT_FILE_INSERT: //insertFornFile(line);
+                printf("Entrei no insert\n");
+                insertProduct(dataFile, readProductDataFromInputFile(dataFile));
                 break;
-            case 'A': modifyFornFile(line);
+            case INPUT_FILE_MODIFY: //modifyFornFile(line);
+                printf("Entrei no modify\n");
+
+                fscanf(inputFile, "%*[^\n]%*r");
                 break;
-            case 'R': removeFromFile(line);
+            case INPUT_FILE_REMOVE: //removeFromFile(line);
+                printf("Entrei no remove\n");
+                int code;
+                fscanf(inputFile, "%d%*r", &code);
+                removeProduct(dataFile, code);
+                break;
+            case WHITE_SPACE:
+                printf("Estou excluindo espaco\n");
+                while (fscanf(inputFile, "%c", &c) == WHITE_SPACE) {
+                    ;
+                }
                 break;
             default:
-                
+                printf("Entrei no default\n");
+                // a linha será ignorada
+                fscanf(inputFile, "%*[^\n]%*r");
+                break;
         }
     }
     fclose(inputFile);
--- src/models/input_file.h
+++ src/models/input_file.h
@@ -1,7 +1,27 @@
+/**
+ * @file input_file.h
+ * @author Victor Emanuel Almeida (victoralmeida2001@hotmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 30/05/2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef INPUT_FILE
 #define INPUT_FILE
 
-#include "tree.h"
+#include "product_tree.h"
+
+#define WHITE_SPACE ' '
+
+enum inputFileOperations {
+    INPUT_FILE_INSERT = 'I',
+    INPUT_FILE_MODIFY = 'A',
+    INPUT_FILE_REMOVE = 'R',
+};
+
+Product * readProductDataFromInputFile(FILE *input);
 
 void loadInputFile(char *inputPath, FILE *dataFile);
 
