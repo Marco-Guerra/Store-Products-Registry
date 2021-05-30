db823d4cc13a17a6684dbe48b099c6d6c74ddd3b
Marco-Guerra <marcoguerrapedroso@gmail.com>
2021-05-30 11:48:01 -0300

    testing isertProduct function

--- src/main.c
+++ src/main.c
@@ -9,18 +9,16 @@ int main(int argc, char *argv[]) {
     if(dataFile == NULL) {
         printf("Estou aqui\n");
         dataFile = makeDataFile(filePath);
+    }else{
+        setbuf(dataFile, NULL);
     }
     
     if(argc == 2) {
         loadInputFile(argv[1], dataFile);
     }
 
-    Head *head = readHead(dataFile);
-    printf("%d\n", head->regRoot);
-    printf("%d\n", head->regFree);
-    printf("%d\n", head->regLast);
-
     mainMenuController(dataFile);
+
     fclose(dataFile);
 
     return 0;
